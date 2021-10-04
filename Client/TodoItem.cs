a080ac27dd7ce74ae769443d297a34eef5286f1d
Matthew Hawley <mhawley2100@eagle.fgcu.edu>
2021-10-04 18:00:38 -0400

    Created TodoItem.cs & added comments

---
+++ Client/TodoItem.cs
@@ -0,0 +1,5 @@
+﻿public class TodoItem
+{
+    public string Title { get; set; } // GETS & SETS the ITEM from Title string
+    public bool IsDone { get; set; } // GETS & SETS the TRUE / FALSE for IsDone boolean
+}
